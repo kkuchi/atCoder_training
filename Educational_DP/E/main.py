3d62a9eb42fb6d3df330cc359e1b325f312e4635
kkuchi <ankeichi.keith@gmail.com>
2021-07-24 17:34:44 +0900

    do edpc to problem I

--- Educational_DP/E/main.py
+++ Educational_DP/E/main.py
@@ -1,44 +1,52 @@
 import sys
 
 def source():
+    spi = lambda x: map(int, x.split())
+
     s = sys.stdin.read().splitlines()
-    n, w = map(int, s[0].split())
-    goods = [list(map(int, i.split())) for i in s[1:]]
+    n, w = spi(s[0])
+    goods = [list(spi(i)) for i in s[1:]]
     return n, w, goods
 
+
+# dp計算
 def calc(n, w, goods):
+    # <dp[要素数][価値] = 重さの最小値> を作成する
     INF = 10 ** 9 + 1
-    cols = sum([i[1] for i in goods])
-    # dp[i][v]は、i-1番目までの品物から価値vになるよう選んだ時の重さの最小値
-    dp = [[0 for _ in range(cols)] for __ in range(n+1)]
-    
+
+    weights = [i[0] for i in goods]
+    values = [i[1] for i in goods]
+
+    value_max = sum(values)+1
+
+    # dp作成、初期値はINF
+    dp = [[INF for _ in range(value_max)] for _ in range(n+1)]
+
+    # 最初だけ0に
+    dp[0][0] = 0
+
+    # dp更新
     for i in range(n):
-        for j in range(cols):
-            print('j', j, 'goods[i][1]', goods[i][1])
-            dp1 = 0
-            if j >= goods[i][1]:
-                dp1 = dp[i][j-goods[i][1]] + goods[i][0]
+        for j in range(value_max):
+            dp1 = dp[i][j-values[i]] + weights[i] if j >= values[i] else INF
             dp2 = dp[i][j]
-            
-            if dp1 == dp2 == 0:
-                pass
-            else:
-                dp1 = INF if dp1 == 0 else dp1
-                dp2 = INF if dp2 == 0 else dp2
-            dp[i+1][j] = min(dp1, dp2)
+            dp3 = dp[i+1][j]
+
+            dp[i+1][j] = min(dp1, dp2, dp3)
+
+    # 最終行からINF以外取り出し
+    last = {val:wei for val, wei in enumerate(dp[n]) if wei <= w}
+
+    # ans取り出し
+    return max(last.keys())
     
-    # 価値:重さ
-    res = {i:v for i,v in enumerate(dp[n]) if v <= w}
-    print(res)
-
-    return max(res.keys())
-
-
+    
 def main():
+    # stdin
     n, w, goods = source()
-
+    # dp計算
     ans = calc(n, w, goods)
-
+    # 結果出力
     print(ans)
 
 
